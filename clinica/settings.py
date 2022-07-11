744688e5cc498dac17a110ec056124013558a6ec
m.iturrieta44 <m.iturrieta44@gmail.com>
2022-07-11 14:02:03 -0400

    agregando settings link

--- clinica/settings.py
+++ clinica/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-sux!#ho@!1sm_&z%d6rg8(hh$_rva22txl@rkl(hrl3pmx-ai4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://mmitur.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
